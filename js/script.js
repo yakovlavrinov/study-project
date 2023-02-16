32c2df1ecb3a18fd7e3a353b64028cf471e2d5fa
Yakov <lawrinov.yackov@yandex.ru>
2023-02-16 12:03:57 +0300

    study project

---
+++ js/script.js
@@ -0,0 +1,30 @@
+import tabs from './modules/tabs';
+import calculator from './modules/calculator';
+import cards from './modules/cards';
+import forms from './modules/forms';
+import modal from './modules/modal';
+import slider from './modules/slider';
+import timer from './modules/timer';
+import {openModal} from './modules/modal';
+
+
+window.addEventListener('DOMContentLoaded', function() {
+   const modalTimerId = setTimeout(() => openModal('.modal', modalTimerId), 300000);
+   tabs('.tabheader__item','.tabcontent', '.tabheader__items', 'tabheader__item_active');
+   calculator();
+   cards();
+   forms('form', modalTimerId);
+   modal('[data-modal]', '.modal', modalTimerId);
+   slider({
+    container: '.offer__slider',
+    nextArrow: '.offer__slider-next',
+    prevArrow: '.offer__slider-prev',
+    slide: '.offer__slide',
+    totalCounter: '#total',
+    currentCounter: '#current',
+    wrapper: '.offer__slider-wrapper',
+    field: '.offer__slider-inner'
+   });
+   timer('.timer', '2023-06-11'); 
+
+});
